c02dca713485a75af852fdd34eb89067368eacf3
Peter Rose <peterroseq1@gmail.com>
2020-03-15 10:30:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 def key_for_min_value(name_hash)
   name_hash = {:chair => 25, :table => 85, :mattress => 450}
-    name_hash.min do |name, value|
-     name[value] < name_hash[value]
+    name_hash.find do |name, value|
+     name[value] < name[value]
   end 
 end
 name_hash = {:chair => 25, :table => 85, :mattress => 450}
